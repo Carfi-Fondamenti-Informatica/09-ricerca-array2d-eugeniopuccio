8360af4108281ba23c4c422e76038b4693a18f51
eugeniopuccio <114490589+eugeniopuccio@users.noreply.github.com>
2022-12-17 01:47:13 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,8 @@
+#ifndef UNTITLED17_LIB_H
+#define UNTITLED17_LIB_H
+void riempi(char [10][20]);
+void riempi2(char[20]);
+bool conf(char[10][20], char [10]);
+void analisi(char a[10][20], char b[20]);
+#endif //UNTITLED17_LIB_H
 
